b08dfec32f0cde97be5e21045d79bb6fe1256760
Alex <dr.advencher@gmail.com>
2021-03-09 17:51:02 +0500

    Update security_config.js

--- config/security_config.js
+++ config/security_config.js
@@ -1,8 +1,8 @@
 let secObj = {
     //валера придумал секретное слово
-    'secret': 'gibiscus',
+    'secret': '',
 
     //александр придумал секретное слово 
-    'codeword': '$2a$04$/le.mbB92s9K1CQIydSZcO4CsIYwEynLc3u4wc5I1s9kSLVVEQGh2'
+    'codeword': ''
 };
 export default secObj;
